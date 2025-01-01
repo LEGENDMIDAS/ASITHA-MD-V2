8c1ef3932c788a24fe9fdf5c4a7600b08e7fe5bb
LEGENDMIDAS <kisakireal@gmail.com>
2025-01-01 20:20:18 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=m34BQS6D#VHg5NqI3W5bTVZIEEIbTC-ezUq9MzQ8iymWpTqVvyMo
+' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
